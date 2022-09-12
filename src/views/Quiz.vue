6eccc785eee144a09826a51326fd4b03389bed27
PAOLORODIL <mpcrodil@tip.edu.ph>
2022-09-12 15:46:36 +0800

    Third commit

--- src/views/Quiz.vue
+++ src/views/Quiz.vue
@@ -13,7 +13,7 @@
       <img id="logotrophy" src="https://i.ibb.co/HKj6V6C/pngegg-1.png" alt="Quiz" />
       <h1 id="headline">Quiz Application - Cartoon & Animation</h1>
       <h5>Current Score: {{score}}/{{questions.length}}</h5>
-      <h5>Question no. {{currentnumberQuestion}}</h5>
+      <h5>Question: {{currentnumberQuestion}} / {{questions.length}}</h5>
   
       <div>
 
@@ -41,127 +41,124 @@
   </template>
     
     <script>
-        export default {
-          name: "Quiz",
-          // data() function stores state variables
-          data() {
-            return {
-              questions: [],
-              loading: true,
-              index: 0,
-              score: 0,
-              currentnumberQuestion: 1,
-            };
+      export default {
+        name: "Quiz",
+        // data() function stores state variables
+        data() {
+          return {
+            questions: [],
+            loading: true,
+            index: 0,
+            score: 0,
+            currentnumberQuestion:1
+          };
+        },
+        computed: {
+          currentQuestion() {
+            if (this.questions !== []) {
+              return this.questions[this.index];
+            }
+            return null;
           },
-          computed: {
-            currentQuestion() {
-              if (this.questions !== []) {
-                return this.questions[this.index];
-              }
-              return null;
-            },
-          },
-      
-          // Custom methods of the Vue Component
-          methods: {
-            async fetchQuestions() {
-              this.loading = true;
-              let response = await fetch(
-                "https://opentdb.com/api.php?amount=15&category=32&difficulty=easy&type=multiple"
-              );
-              let jsonResponse = await response.json();
-              let index = 0; // index is used to identify single answer
-              let data = jsonResponse.results.map((question) => {
-                // put answers on question into single array
-                question.answers = [
-                  question.correct_answer,
-                  ...question.incorrect_answers,
+        },
+        // Custom methods of the Vue Component
+        methods: {
+          async fetchQuestions() {
+            this.loading = true;
+            let response = await fetch(
+              "https://opentdb.com/api.php?amount=15&category=32&difficulty=easy&type=multiple"
+            );
+            let jsonResponse = await response.json();
+            let index = 0; // index is used to identify single answer
+            let data = jsonResponse.results.map((question) => {
+              // put answers on question into single array
+              question.answers = [
+                question.correct_answer,
+                ...question.incorrect_answers,
+              ];
+              // Shuffle question.answers array
+              for (let i = question.answers.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+                [question.answers[i], question.answers[j]] = [
+                  question.answers[j],
+                  question.answers[i],
                 ];
-                // Shuffle question.answers array
-                for (let i = question.answers.length - 1; i > 0; i--) {
-                  const j = Math.floor(Math.random() * (i + 1));
-                  [question.answers[i], question.answers[j]] = [
-                    question.answers[j],
-                    question.answers[i],
-                  ];
-                }
-                // add rightAnswer and key property to each question
-                question.rightAnswer = null;
-                question.key = index;
-                index++;
-                return question;
-              });
-              this.questions = data;
-              this.loading = false;
-            },
-            
-            handleButtonClick: function (event) {
-              /* Find index to identiy question object in data */
-              let index = event.target.getAttribute("index");
-        
-              let pollutedUserAnswer = event.target.innerHTML; // innerHTML is polluted with decoded HTML entities e.g ' from &#039;
-              /* Clear from pollution with ' */
-              let userAnswer = pollutedUserAnswer.replace(/'/, "&#039;");
-        
-              /* Set userAnswer on question object in data */
-              this.questions[index].userAnswer = userAnswer;
-        
-              /* Set class "clicked" on button with userAnswer -> for CSS Styles; Disable other sibling buttons */
-              event.target.classList.add("clicked");
-              let allButtons = document.querySelectorAll(`[index="${index}"]`);
-        
-              for (let i = 0; i < allButtons.length; i++) {
-                if (allButtons[i] === event.target) continue;
-        
-                allButtons[i].setAttribute("disabled", "");
               }
-        
-              /* Invoke checkAnswer to check Answer */
-              this.checkAnswer(event, index);
-            },
-            checkAnswer: function (event, index) {
-              let question = this.questions[index];
-              
-        
-              if (question.userAnswer) {
-                if (this.index < this.questions.length - 1) {
-                  setTimeout(
-                    function () {
-                      this.index += 1;
-                    }.bind(this),
-                    3000
-                    
-                  );
-                }
-                if (question.userAnswer === question.correct_answer) {
-                  /* Set class on Button if user answered right, to celebrate right answer with animation joyfulButton */
-                  event.target.classList.add("rightAnswer");
-                  /* Set rightAnswer on question to true, computed property can track a streak out of 10 questions */
-                  this.questions[index].rightAnswer = true;
-                  this.score++;
-                } else {
-                  /* Mark users answer as wrong answer */
-                  event.target.classList.add("wrongAnswer");
-                  this.questions[index].rightAnswer = false;
-                  /* Show right Answer */
-                  let correctAnswer = this.questions[index].correct_answer;
-                  let allButtons = document.querySelectorAll(`[index="${index}"]`);
-                  allButtons.forEach(function (button) {
-                    if (button.innerHTML === correctAnswer) {
-                      button.classList.add("showRightAnswer");
-                    }
-                  });
-                }
+              // add rightAnswer and key property to each question
+              question.rightAnswer = null;
+              question.key = index;
+              index++;
+              return question;
+            });
+            this.questions = data;
+            this.loading = false;
+          },
+          handleButtonClick: function (event) {
+            /* Find index to identiy question object in data */
+            let index = event.target.getAttribute("index");
+      
+            let pollutedUserAnswer = event.target.innerHTML; // innerHTML is polluted with decoded HTML entities e.g ' from &#039;
+            /* Clear from pollution with ' */
+            let userAnswer = pollutedUserAnswer.replace(/'/, "&#039;");
+      
+            /* Set userAnswer on question object in data */
+            this.questions[index].userAnswer = userAnswer;
+      
+            /* Set class "clicked" on button with userAnswer -> for CSS Styles; Disable other sibling buttons */
+            event.target.classList.add("clicked");
+            let allButtons = document.querySelectorAll(`[index="${index}"]`);
+      
+            for (let i = 0; i < allButtons.length; i++) {
+              if (allButtons[i] === event.target) continue;
+      
+              allButtons[i].setAttribute("disabled", "");
+            }
+      
+            /* Invoke checkAnswer to check Answer */
+            this.checkAnswer(event, index);
+          },
+          checkAnswer: function (event, index) {
+            let question = this.questions[index];
+      
+            if (question.userAnswer) {
+              if (this.index < this.questions.length - 1) {
+                setTimeout(
+                  function () {
+                    this.index += 1;
+                    this.currentnumberQuestion+=1;
+                  }.bind(this),
+                  1000
+                );
               }
-            },
+              if (question.userAnswer === question.correct_answer) {
+                /* Set class on Button if user answered right, to celebrate right answer with animation joyfulButton */
+                event.target.classList.add("rightAnswer");
+                /* Set rightAnswer on question to true, computed property can track a streak out of 10 questions */
+                this.questions[index].rightAnswer = true;
+                this.score++;
+              } else {
+                /* Mark users answer as wrong answer */
+                event.target.classList.add("wrongAnswer");
+                this.questions[index].rightAnswer = false;
+                /* Show right Answer */
+                let correctAnswer = this.questions[index].correct_answer;
+                let allButtons = document.querySelectorAll(`[index="${index}"]`);
+                allButtons.forEach(function (button) {
+                  if (button.innerHTML === correctAnswer) {
+                    button.classList.add("showRightAnswer");
+                  }
+                });
+              }
+            }
           },
-          
-          // Code inside mounted() runs after the Component has mounted
-          mounted() {
-            this.fetchQuestions();
-          },
-        };
-        </script>
+        },
+        // Code inside mounted() runs after the Component has mounted
+        mounted() {
+          this.fetchQuestions();
+        },
+      };
+      </script>
+
     
     <style scoped>
   #quiz-container {
